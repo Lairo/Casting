cc6a7583a12e8ddf5f41265ee9b5241ae6d8e9d9
chajus1 <chajus1@googlemail.com>
2023-05-24 10:25:26 +0100

    more

--- Casting/Program.cs
+++ Casting/Program.cs
@@ -12,7 +12,18 @@ namespace Casting
         {
             byte myFloatValue = 255;
             int myIntValue = (int)myFloatValue;
-            Console.WriteLine("myIntValue is " + myIntValue);
+            //Console.WriteLine("myIntValue is " + myIntValue);
+            int myInt = 10;
+            int myString = myInt;
+            byte myByte = (byte)myInt;
+            double myDouble = (double)myByte;
+            char myChar = 'v';
+            myString = (int)(myString + myInt + myByte + myDouble + myChar);
+
+
+
+            Console.WriteLine($"\n{myString}\n");
+
         }
     }
 }
