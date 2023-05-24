f80a483a7056c7b9cdfbfcd96b16b4b695aa550e
chajus1 <chajus1@googlemail.com>
2023-05-24 09:49:48 +0100

    Add project files.

---
+++ Casting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myFloatValue = 255;
+            int myIntValue = (int)myFloatValue;
+            Console.WriteLine("myIntValue is " + myIntValue);
+        }
+    }
+}
